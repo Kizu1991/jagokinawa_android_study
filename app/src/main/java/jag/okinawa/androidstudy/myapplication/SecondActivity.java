f13bc5f90a8f154ddddf5d43d444984ea267af02
k-kamiya <k-kamiya@protosolution.co.jp>
2017-10-24 01:10:22 +0900

    日付表示処理、暗黙的Intent処理追加

--- app/src/main/java/jag/okinawa/androidstudy/myapplication/SecondActivity.java
+++ app/src/main/java/jag/okinawa/androidstudy/myapplication/SecondActivity.java
@@ -1,6 +1,7 @@
 package jag.okinawa.androidstudy.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,6 +11,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import jag.okinawa.androidstudy.myapplication.models.Event;
 import jag.okinawa.androidstudy.myapplication.models.Eventon;
@@ -34,15 +40,6 @@ public class SecondActivity extends AppCompatActivity {
 
         mProgressBar = (ProgressBar) findViewById(R.id.progress);
         mProgressBar.setVisibility(View.VISIBLE);
-
-//        findViewById(R.id.finish).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                setResult(Activity.RESULT_OK);
-//                finish();
-//            }
-//        });
-
 
         API api = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
@@ -77,6 +74,16 @@ public class SecondActivity extends AppCompatActivity {
         TextView address = (TextView) findViewById(R.id.address);
         WebView contents = (WebView) findViewById(R.id.contents);
 
+        // ブラウザ起動
+        findViewById(R.id.brawser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(mEvent.getEventUrl());
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(i);
+            }
+        });
+
         // タイトル設定
         title.setText(mEvent.getTitle());
         // 画像設定
@@ -89,14 +96,40 @@ public class SecondActivity extends AppCompatActivity {
             capacityStr += " キャンセル待ち：" + mEvent.getWaiting();
         }
         capacity.setText(capacityStr);
+
+        // 日付フォーマッター
+        String SDF_W3C_TO_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+        SimpleDateFormat sdf = new SimpleDateFormat(SDF_W3C_TO_FORMAT, Locale.getDefault());
+        String SDF_STR_FORMAT = "yyyy/MM/dd HH:mm";
+        SimpleDateFormat sdfConvert = new SimpleDateFormat(SDF_STR_FORMAT, Locale.getDefault());
+
+        // 日付情報変換
+        String startAtStr = "";
+        String endAtStr = "";
+
+        try {
+            Date date = sdf.parse(mEvent.getStartedAt());
+            startAtStr = sdfConvert.format(date);
+
+            date = sdf.parse(mEvent.getEndedAt());
+            endAtStr = sdfConvert.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         // 開催日
-        startAt.setText("開催日：" + mEvent.getStartedAt());
+        startAt.setText("開催日：" + startAtStr);
         // 終了日
-        endedAt.setText("終了日：" + mEvent.getEndedAt());
+        endedAt.setText("終了日：" + endAtStr);
         // 開催場所
         address.setText("開催場所：" + mEvent.getAddress());
         // 詳細
-        contents.loadDataWithBaseURL("", mEvent.getContents(), "text/html", "UTF-8", "");
+        contents.loadDataWithBaseURL(
+                "",
+                mEvent.getContents(),
+                "text/html",
+                "UTF-8",
+                "");
     }
 
 }
